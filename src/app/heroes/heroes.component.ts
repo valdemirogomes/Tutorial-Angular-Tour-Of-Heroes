f1bebd2231493e033605b3c57bc43dfbb5934aa8
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-10-08 14:12:28 -0300

    Exibindo lista de Heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 
 @Component({
@@ -9,13 +10,22 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes = HEROES;
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+  
   hero : Hero = {
     id: 1,
     name: "Windstorm"
   } 
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
 
 }
