ef4f99a06e5dcd2351c359dfa2dcd5776c2c8169
Valdemiro Gomes <valdemiro.gomes.rodrigues@gmail.com>
2022-10-14 01:30:21 -0300

    Ïmplementação do observable

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -17,9 +17,10 @@ export class HeroesComponent implements OnInit {
 ngOnInit(): void {
   this.getHeroes();
 }
-  getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
-  }
+getHeroes(): void {
+  this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+}
   
 
 
